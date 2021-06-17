229c6d2788470394b7fc4090f1276cb58f0dcdd0
unknown <ba18048@ues.edu.sv>
2021-06-17 09:39:56 -0600

    creacion de proyecto y migraciones

---
+++ HDP115/principal/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+class persona(models.Model):
+    idpersona = models.AutoField(primary_key=True)
+    dui = models.CharField(max_length=9,unique=True)
+    nombre = models.CharField(max_length= 50)
+    apellido = models.CharField(max_length=50)
+    genero = models.CharField(max_length=1)
+    numeroTelefono = models.CharField(max_length=50)
+    correoElectronico = models.EmailField(max_length=254)
+    rol = models.IntegerField()
+
+class usuario(models.Model):
+    idUsuario = models.AutoField(primary_key=True)
+    idpersona = models.ForeignKey(persona, on_delete=models.CASCADE)
+    contrasena = models.CharField(max_length=50)
+
+class administrador(models.Model):
+    idAdministrador = models.AutoField(primary_key=True)
+    idpersona = models.ForeignKey(persona, on_delete=models.CASCADE)
+    contrasena = models.CharField(max_length=50)
+
+class zona(models.Model):
+    codigoZona = models.AutoField(primary_key=True)
+    departamento = models.CharField(max_length=50)
+    municipio = models.CharField(max_length=50,unique=True)
+
+class asignacion(models.Model):
+    idcomprobante = models.AutoField(primary_key=True)
+    idAdmistrador = models.ForeignKey(administrador, on_delete=models.CASCADE)
+    idUsuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    codigoZona = models.ForeignKey(zona, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fechaDeAsignacion = models.DateField()
+
+
+class paqueteAlimentario(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    idAdministrador = models.ForeignKey(administrador, on_delete=models.CASCADE)
+    fechaDeExpedicion = models.DateField()
+    fechaDeCaducidad = models.DateField()
